b83dccac0b733612ea414a535441551e0054c053
Maratovich <snowsaber@mail.ru>
2022-07-14 18:10:33 +0400

    directoryPositive

---
+++ src/test/java/test/directoryPositive.java
@@ -0,0 +1,39 @@
+package test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
+
+public class directoryPositive {
+
+    @BeforeAll
+    static void beforeAll() {
+       browserSize = "1920x1080";
+    }
+
+    @Test
+    void directoryTest() throws InterruptedException {
+
+        //Адрес
+        open("http://85.113.47.244:21480");
+
+        //Вход на сайт
+        $("#inputLogin").setValue("admin");
+        $("#inputPassword").setValue("Admin777!");
+        $("#loginButton").click();
+
+        //Вход в справочники
+
+
+
+
+
+    }
+
+
+}
